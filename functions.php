a3195d53134eb09982cdc82b64ada018f2a76468
Florian <26717601+fbthb@users.noreply.github.com>
2021-07-17 18:15:54 +0200

    allow REST-API

--- functions.php
+++ functions.php
@@ -154,11 +154,11 @@ function shapeSpace_check_enum($redirect, $request) {
 
 
 // Disable REST 
-add_filter('rest_authentication_errors', function($result) {
+/*add_filter('rest_authentication_errors', function($result) {
   if (!is_user_logged_in()) {
     return new WP_Error('rest_API_cannot_access', array( 'status' => rest_authorization_required_code()));
     return $result;
   }
-});
+});*/
 	
 ?>
