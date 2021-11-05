838f995f1a2edc2d4c0dab26947747e94496ccd7
Florian <26717601+fbthb@users.noreply.github.com>
2021-11-05 14:39:07 +0100

    restrict enumeration of users with rest-api

--- functions.php
+++ functions.php
@@ -152,5 +152,14 @@ function shapeSpace_check_enum($redirect, $request) {
 	else return $redirect;
 }
 
+add_filter( 'rest_endpoints', function( $endpoints ){
+    if ( isset( $endpoints['/wp/v2/users'] ) ) {
+        unset( $endpoints['/wp/v2/users'] );
+    }
+    if ( isset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] ) ) {
+        unset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] );
+    }
+    return $endpoints;
+});
 	
 ?>
